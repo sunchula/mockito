8a34033d407f4ac25858f4832001671e466ba8ce
nitsolution2021 <praveen@nitsolution.in>
2022-01-19 16:12:01 +0530

    Added Service and IMPL Class

---
+++ src/main/java/org/praveen/mockito/service/TodoService.java
@@ -0,0 +1,12 @@
+package org.praveen.mockito.service;
+
+import java.util.List;
+
+//External Service - Lets say this comes from WunderList
+public interface TodoService {
+
+	public List<String> retrieveTodos(String user);
+
+//	void deleteTodo(String todo);
+
+}
