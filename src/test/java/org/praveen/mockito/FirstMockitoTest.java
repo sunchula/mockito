060358e8500acab4b47a4c74052f6140e46198f5
nitsolution2021 <praveen@nitsolution.in>
2022-01-19 15:51:10 +0530

    Sample Test File

---
+++ src/test/java/org/praveen/mockito/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package org.praveen.mockito;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
